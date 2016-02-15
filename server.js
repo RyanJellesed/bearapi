2d4aec79cf3cc744f685dc788a5ce415ca0c9621
RyanJellesed <ryan_jellesed@hotmail.com>
2016-02-15 12:00:22 -0700

    added the bears

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
+var monte = {name: 'monte',id: '1',age: '32',gender: 'male'};
+var smokey = {name: 'smokey',id: '2',age: '80',gender: 'male'};
+var winnie = {name: 'winni',id: '3',age: '100',gender: 'male'};
+
+var bears = [monte,smokey,winnie];
+
 app.get('/api/bears', function(req, res) {
-	res.json({
-		title: 'does it work?', 
-		body: 'I hope so..'})
+	res.json(bears);
 });
 
 
