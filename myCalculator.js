b34d4000b17b35faa923693f48211a566840bc36
RyanJellesed <ryan_jellesed@hotmail.com>
2016-02-15 10:29:35 -0700

    created Calculator Object, and myCalculator.js file

---
+++ myCalculator.js
@@ -0,0 +1,17 @@
+var Calculator = require('./calculator');
+
+console.log(myCalculator);
+
+
+var myCalculator = new Calculator();
+
+myCalculator.sum(4,9);
+
+
+//describe('Calculator', function () {
+//	var calculator = new Calculator();
+
+
+
+
+
