0861ad2a826ec8d4279feca47e0b366668517c53
RyanJellesed <ryan_jellesed@hotmail.com>
2016-02-15 11:27:10 -0700

    ...

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-npm install express --save
+var express = require('express');
+var app = express();
+
+app.get('/api/bears', function(req, res) {
+	res.json({
+		title: 'does it work?', 
+		body: 'I hope so..'})
+});
+
+
+app.listen(3000, function() {
+	console.log('the server is up and running..')
+});
+
